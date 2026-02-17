a1afe3f3ce2733f8ef50f4965e51e25697c62cb0
Rory Quinn <n00255243@iadt.ie>
2026-02-17 13:38:29 +0000

    step 8 done

--- src/exercises/05-php-database/classes/Book.php
+++ src/exercises/05-php-database/classes/Book.php
@@ -41,11 +41,27 @@ class Book
     // =========================================================================
     // Exercise 8: Book Class Basics
     // =========================================================================
-    public function __construct($data = [])
-    {
+
         // TODO: Get database connection from DB singleton
         // TODO: If $data is not empty, populate properties using null coalescing operator
+    public function __construct($data = [])
+    {
+        $this->db = DB::getInstance()->getConnection();
+
+            if (!empty($data)) {
+                $this->id = $data['id'] ?? null;
+                $this->title = $data['title'] ?? null;
+                $this->author = $data['author'] ?? null;
+                $this->publisher_id = $data['publisher_id'] ?? null;
+                $this->year = $data['year'] ?? null;
+                $this->isbn = $data['isbn'] ?? null;
+                $this->description = $data['description'] ?? null;
+                $this->cover_filename = $data['cover_filename'] ?? null;
+                }
     }
+
+
+
 
     // =========================================================================
     // Exercise 9: Finder Methods
@@ -90,8 +106,17 @@ class Book
     // =========================================================================
     // Exercise 8: Book Class Basics
     // =========================================================================
-    public function toArray()
-    {
-        // TODO: Implement this method
-    }
+   public function toArray()
+                {
+                    return [
+                        'id' => $this->id,
+                        'title' => $this->title,
+                        'author' => $this->author,
+                        'publisher_id' => $this->publisher_id,
+                        'year' => $this->year,
+                        'isbn' => $this->isbn,
+                        'description' => $this->description,
+                        'cover_filename' => $this->cover_filename
+                    ];
+                }
 }
